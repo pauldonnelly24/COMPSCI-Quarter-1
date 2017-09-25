14678eee75a96272936765f6b4a4f4e4828f0a32
pauldonnelly24 <31353102+pauldonnelly24@users.noreply.github.com>
2017-09-24 22:50:17 -0400

    Update FirstLastDigit.java

--- FirstLastDigit.java
+++ FirstLastDigit.java
@@ -7,7 +7,7 @@ package Quarter1;
 import java.util.Scanner;
 /**
  *
- * @author Owner
+ * @author PJJJ
  */
 public class FirstLastDigit {
     public static void main(String[] args){
@@ -19,28 +19,7 @@ public class FirstLastDigit {
         char a = intString.charAt(0);
         char z = intString.charAt(intString.length() - 1);
      System.out.println("First digit is " + a + " and the last digit is " + z);
-        /*int num = in.nextInt();
-
-String numString = String.valueOf(num);
-char first = numString.charAt(0);
-char last = numString.charAt(numString.length() - 1);
-
-System.out.println("The first digit is " + first + ", and the last digit is " + last);*/
-        
-       
-    int fdig, ldig, num;
-
-    System.out.println("Enter an Integer.");
-    num = in.nextInt();
-
-    ldig = num%10;
-    fdig = num/10;
-    while (num > 0)
-    { 
-        num = num/10;
-    }
-         System.out.println("The first digit is " + fdig + ", and the last digit is " + ldig + ".");
-         
+    
 }
     }
     
